b0891ff127c803ef256b9e189e0dda9f6cae9934
Sebastian Br <sebastian.brancacho@gmail.com>
2024-03-13 10:57:34 -0500

    nav-bar apple store

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
+import './assets/fonts/index.css';
+import './css/style.css'
 
 createApp(App).use(store).mount('#app')
